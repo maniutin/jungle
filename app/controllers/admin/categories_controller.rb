3fb130ed0eb000fd74835c23d91e985a7d4b1865
Nick Maniutin <maniutin@gmail.com>
2021-03-31 17:40:58 +0000

    add the ability to create new categories

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -2,9 +2,7 @@ class Admin::CategoriesController < ApplicationController
   http_basic_authenticate_with :name => ENV['ADMIN_USERNAME'], :password => ENV['ADMIN_PASSWORD']
   def index
     @categories = Category.order(id: :desc).all
-
   end
-
 
   def new
     @category = Category.new
@@ -18,5 +16,13 @@ class Admin::CategoriesController < ApplicationController
     else
       render :new
     end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(
+      :name
+    )
   end
 end
